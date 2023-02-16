4ea178ac77c8592f1d3b19d429ad57d13d6f69b7
RehamDeghady20 <120522265+RehamDeghady20@users.noreply.github.com>
2023-02-16 21:33:29 +0200

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,6 @@
 #include <stdio.h>
-<<<<<<< HEAD
-=======
 int main (void)
 { 
-  puts ("Programming is like building a multilingual puzzle"\");
+  puts ("\"Programming is like building a multilingual puzzle");
         return (0);
         }
->>>>>>> 4db99b47540af7340a93e80adc2a0b9c53292e7d
