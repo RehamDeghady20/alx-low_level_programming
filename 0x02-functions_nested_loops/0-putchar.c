90696cd27028ad2a406d1d6db7e44c24285763a2
RehamDeghady20 <120522265+RehamDeghady20@users.noreply.github.com>
2023-02-23 02:22:04 +0200

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,17 @@
 #include "holberton.h"
 /**
-*main - prints Holberton
+*main - prints putchar
 *Return: 0
 */
 int main(void)
 {
-	_putchar('H');
-	_putchar('o');
-	_putchar('l');
-	_putchar('b');
-	_putchar('e');
-	_putchar('r');
+	_putchar('p');
+	_putchar('u');
 	_putchar('t');
-	_putchar('o');
-	_putchar('n');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 	return (0);
 }
